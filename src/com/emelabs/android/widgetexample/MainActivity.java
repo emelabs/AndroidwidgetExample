2dfda6d3fe11b44f2a147473c0eba7e36c0f1d85
thano <mauricio.ponce.o@gmail.com>
2013-12-09 15:14:38 -0300

    [ADD] store offset in sharedpreferences

--- src/com/emelabs/android/widgetexample/MainActivity.java
+++ src/com/emelabs/android/widgetexample/MainActivity.java
@@ -4,15 +4,21 @@ package com.emelabs.android.widgetexample;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
 	private static final String TAG = "MainActivity";
 	private int widgetId;
+	
+	private EditText edOffset;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,15 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		try{
+			edOffset = (EditText) findViewById(R.id.editText1);
+			
 			widgetId = getIntent().getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, 0);
 			Log.d(TAG, "[onCreate] widgetId: " + widgetId );
+
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			int offset = prefs.getInt(MyWidgetProvider.PREF_KEY_OFFSET, 1);
+			edOffset.setText(String.valueOf(offset));
+			
 		}catch(Exception e){
 			Log.e(TAG, "[onCreate]", e);
 		}
@@ -40,13 +53,21 @@ public class MainActivity extends Activity {
 	public void onSave(View view){
 		Log.v(TAG, "[onSave] entering...");
 		
+		try{
+			Log.v(TAG, "[onSave] offset:" + edOffset.getText().toString());
+			
+			Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+			editor.putInt(MyWidgetProvider.PREF_KEY_OFFSET, Integer.valueOf(edOffset.getText().toString()));
+			editor.commit();
+		}catch(Exception e){
+			Log.e(TAG, "[onSave]", e);
+		}
+		
 		Intent intent = new Intent(this,MyWidgetProvider.class);
 		intent.setAction("android.appwidget.action.APPWIDGET_UPDATE");
 
-		// Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
-		// since it seems the onUpdate() is only fired on that:
 		int[] ids = {widgetId};
-		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,ids);
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
 		sendBroadcast(intent);
 		
 		finish();
--- src/com/emelabs/android/widgetexample/MyWidgetProvider.java
+++ src/com/emelabs/android/widgetexample/MyWidgetProvider.java
@@ -8,6 +8,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -15,24 +17,27 @@ public class MyWidgetProvider extends AppWidgetProvider {
 	
 	private static final String TAG = "MyWidgetProvider";
 	
-	private long seed;
+	public static final String PREF_KEY_OFFSET = "key_offset";
 	
+	private int offset = 1;
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		Log.d(TAG, "[onUpdate] ");
 
+		loadOffsetFromPreferences(context);
+		
 		// Get all ids
 		ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
 		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 		for (int widgetId : allWidgetIds) {
 			
 			// Create some random data
-			int number = (new Random().nextInt(100));
+			int number = (new Random().nextInt(100)) + offset;
 
 			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-			Log.i(TAG, "[onUpdate] " + String.valueOf(number));
+			Log.i(TAG, "[onUpdate] offset:" + offset + " | value:" + String.valueOf(number));
 			
 			// Set the text
 			remoteViews.setTextViewText(R.id.update, String.valueOf(number));
@@ -48,5 +53,15 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		}
 		
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+	
+	private void loadOffsetFromPreferences(Context context){
+		try{
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+			offset = prefs.getInt(PREF_KEY_OFFSET, offset);
+		}catch(Exception e){
+			Log.e(TAG, "[loadOffsetFromPreferences]", e);
+		}
+		Log.v(TAG, "[loadOffsetFromPreferences] offset:" + offset);
 	}
 }
